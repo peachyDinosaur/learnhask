ad190dbc0bffc6ead9de3eebaff01dbbaf381ddc
peachyDinosaur <eliaskelly@live.ie>
2020-07-17 17:15:17 +0100

    finished happy learn haskell

--- hlh.hs
+++ hlh.hs
@@ -500,3 +500,291 @@ bornFirst people =
     L.minimumBy compareBirthYears people
   where compareBirthYears x y =
           compare (yearOfBirth x) (yearOfBirth y)
+        
+
+-- CHAPTER 18
+
+type Level = (Integer, Integer)
+
+levels :: [Level]
+levels =
+    concat $ map pairForNums [3,5..12]
+    where
+        pairForNums num = zip[2..12] $ repeat num 
+
+levelNumber :: [a] -> Int
+levelNumber remainingLevels = 
+    totalLevels - levelsLeft
+  where totalLevels = length levels + 1
+        levelsLeft  = length remainingLevels
+    
+
+
+main2 :: IO ()
+main2 = do
+  putStrLn "Suddenly, you wake up. Oh no, you're on..."
+  putStrLn "The Times-Table Train of Terror!"
+  putStrLn "Try to get to the end. We DARE you!"
+  trainLoop levels
+
+trainLoop :: [Level] -> IO()
+trainLoop [] = 
+    putStrLn "you beat the game"
+trainLoop remainingLevels @ (currentLevel : levelsAfterThisOne) =
+    do
+        let currentLevelNumber = 
+                levelNumber remainingLevels
+            (num1, num2) =
+                currentLevel
+        putStrLn $
+            "You are in a Train Carraige "
+            ++ show currentLevelNumber
+            ++ " of " ++ (show $ length levels)
+        putStrLn "Do you want to:"
+        putStrLn "1. Go to next carraige"
+        putStrLn "2. Jump out of the train"
+        putStrLn "3. Eat some food"
+        putStrLn "q Quit"
+        activity <- getLine
+        case activity of
+            "1" ->
+                do
+                    putStrLn $ "You try to go to the next carriage."
+                             ++"The Door is locked"
+                    putStrLn "Answer this question to open the door"
+                    putStrLn $ "What is " ++ show num1
+                             ++ " times " ++ show num2 ++ "?"
+                    answer <- getLine
+                    if answer == (show $ num1 * num2)
+                    then do
+                        putStrLn "The door is open now"
+    
+                        trainLoop levelsAfterThisOne
+                    else do
+                        putStrLn $ "Wrong. You try to open the lock,"
+                                ++ " but it won't open."
+                        trainLoop remainingLevels
+            "2" -> jumpingFutility
+            "3" -> eatingFutility
+            "q" -> putStrLn $ "You decide to quit."
+                            ++ " Thanks for playing. Bah-Bye!"
+            _   -> do
+                putStrLn "That makes NO sense! Try again."
+                trainLoop remainingLevels           
+
+jumpingFutility :: IO ()
+jumpingFutility = do
+  putStrLn "You try to jump out of the train."
+  putStrLn "You fail and die."
+  trainLoop levels
+
+eatingFutility :: IO ()
+eatingFutility = do
+  putStrLn "You see a delicious looking cupcake."
+  putStrLn "You eat it. It's a time travel cupcake!"
+  trainLoop levels
+
+printBirthday :: IO()
+printBirthday = do
+    putStrLn "what year were you born"
+    answer <- getLine
+    putStrLn $ "so your birthday is "++ answer
+
+-- CHAPTER 19
+-- Really convoluted exercise/chp
+
+data GameObject = Player
+                | Acorn
+  deriving (Eq, Show)
+data Room =
+  Room Description [GameObject]
+    deriving (Show)
+type Description = String
+type Inventory = [GameObject]
+type GameMap = [Room]
+type GameState = (GameMap, Inventory)
+
+
+initialState :: GameState
+initialState =
+  ( [ Room "You are inside a tree." [Player]
+    , Room "You are outside of a tree." [Acorn]]
+  , [] )
+
+main3 :: IO ()
+main3 = do
+  putStrLn "Welcome to Skwak the Squirrel."
+  putStrLn "You are a squirrel."
+  gameLoop initialState
+
+gameLoop :: GameState -> IO ()
+gameLoop (rooms, currentInv) = do
+  let currentRoom =
+        case findRoomWithPlayer rooms of
+        Just r -> r
+        Nothing -> error $ "Somehow the player "
+                         ++ "ended up outside the map!"
+      possibleCmds =
+        validCommands currentRoom currentInv
+  if playerWon (rooms, currentInv)
+  then gameOverRestart
+  else do
+    describeWorld currentRoom currentInv possibleCmds
+    takeActionThenLoop
+      currentRoom currentInv possibleCmds rooms
+
+findRoomWithPlayer :: [Room] -> Maybe Room
+findRoomWithPlayer rooms =
+  L.find (\(Room _ obs) ->
+            any (== Player) obs)
+         rooms
+
+validCommands :: Room -> Inventory -> [String]
+validCommands (Room _ gameObjs) invItems =
+    ["go"] ++ takeCommandList
+    ++ dropCommandList ++ ["quit"]
+  where
+    takeCommandList =
+      if somethingToTake gameObjs
+      then ["take"]
+      else []
+    dropCommandList =
+      if length invItems > 0
+      then ["put"]
+      else []
+
+somethingToTake :: [GameObject] -> Bool
+somethingToTake objs =
+  any (/= Player) objs
+
+
+playerWon :: GameState -> Bool
+playerWon (rooms, currentInv) =
+    any hasAcornAndInside rooms
+  where hasAcornAndInside (Room desc objs) =
+          desc == "You are inside a tree."
+          && any (==Acorn) objs
+
+
+gameOverRestart :: IO ()
+gameOverRestart = do
+  putStrLn $ "You won!"
+    ++ "You have successfully stored the acorn"
+    ++ " for winter. Well done!"
+  putStrLn "Do you want to play again? y = yes"
+  playAgain <- getLine
+  if playAgain == "y"
+  then gameLoop initialState
+  else putStrLn "Thanks for playing!"
+
+getCommand :: IO String
+getCommand = do
+  putStrLn "What do you want to do?"
+  getLine
+
+takeActionThenLoop :: Room ->
+                      Inventory ->
+                      [String] ->
+                      [Room] ->
+                      IO ()
+takeActionThenLoop currentRoom
+                   currentInv
+                   possibleCmds
+                   rooms =
+  do
+    command <- getCommand
+    if any (==command) possibleCmds
+    then case command of
+      "go" ->
+          do
+            putStrLn "You go..."
+            gameLoop $ movePlayer (rooms, currentInv)
+      "take" ->
+          do
+            putStrLn "You take the acorn..."
+            gameLoop $
+              moveAcornToInventory (rooms, currentInv)
+      "put" ->
+          do
+            putStrLn "You put the acorn down..."
+            gameLoop $
+              moveAcornFromInventory (rooms, currentInv)
+      "quit" ->
+          putStrLn $ "You decide to give up."
+                   ++ " Thanks for playing."
+      _ ->
+          do
+            putStrLn "That is not a command."
+            gameLoop (rooms, currentInv)
+    else do
+      putStrLn $ "Command not possible here,"
+               ++ " or that is not a command."
+      gameLoop (rooms, currentInv)
+
+movePlayer :: GameState -> GameState
+movePlayer (rooms, inv) =
+    (newRooms, inv)
+  where
+    newRooms =
+      map adjustRooms rooms
+    adjustRooms (Room d objs) =
+      if any (==Player) objs
+      then (Room d (filter (/=Player) objs))
+      else (Room d (Player : objs))
+
+moveAcornToInventory :: GameState -> GameState
+moveAcornToInventory (rooms, inv) =
+    (newRooms, newInv)
+  where
+    newInv =
+      Acorn : inv
+    newRooms =
+      map adjustRooms rooms
+    adjustRooms (Room d objs) =
+      Room d (filter (/=Acorn) objs)
+
+moveAcornFromInventory :: GameState -> GameState
+moveAcornFromInventory (rooms, inv) =
+    (newRooms, newInv)
+  where
+    newInv =
+      filter (/=Acorn) inv
+    newRooms =
+      map adjustRooms rooms
+    adjustRooms (Room d objs) =
+      if any (==Player) objs
+      then Room d (Acorn : objs)
+      else Room d objs
+
+describeWorld :: Room ->
+                 Inventory ->
+                 [String] ->
+                 IO ()
+
+describeWorld currentRoom
+              currentInv
+              possibleCmds =
+  do
+    putStrLn $ describeRoom currentRoom
+    putStrLn $ describeInventory currentInv
+    putStrLn $ describeCommands possibleCmds
+
+
+describeRoom :: Room -> String
+describeRoom (Room desc objs) =
+  desc ++ if any (==Acorn) objs
+          then " There is an acorn here"
+          else ""
+
+describeInventory :: Inventory -> String
+describeInventory [] =
+  "You are holding nothing"
+describeInventory inv =
+  "You are holding: " ++
+  (concat $ map show inv)
+
+describeCommands :: [String] -> String
+describeCommands commands =
+  "Commands: "
+  ++ (L.intercalate ", " commands)
+
